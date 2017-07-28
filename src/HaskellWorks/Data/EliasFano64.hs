1491bf5f6351a95d4bfa2b66183c444267970b5f
John Ky <newhoggy@gmail.com>
2017-07-28 22:25:34 +1000

    Upgrade tests to hedgehog

--- src/HaskellWorks/Data/EliasFano64.hs
+++ src/HaskellWorks/Data/EliasFano64.hs
@@ -4,26 +4,35 @@ module HaskellWorks.Data.EliasFano64
   ( EliasFano64(..)
   , FromEliasFano64(..)
   , ToEliasFano64(..)
+  , FromListWord64(..)
+  , ToListWord64(..)
   ) where
 
-import qualified Data.Vector.Storable                   as DVS
-import           Data.Word
-import           HaskellWorks.Data.AtIndex
-import           HaskellWorks.Data.Bits.BitWise
-import           HaskellWorks.Data.Bits.Log2
-import           HaskellWorks.Data.EliasFano64.Internal
-import           HaskellWorks.Data.PackedVector         as PV
-import           HaskellWorks.Data.Positioning
-import           HaskellWorks.Data.Take
-import           Safe
-import           Prelude hiding (length, take)
+import Data.Word
+import HaskellWorks.Data.AtIndex
+import HaskellWorks.Data.Bits.BitWise
+import HaskellWorks.Data.Bits.Log2
+import HaskellWorks.Data.EliasFano64.Internal
+import HaskellWorks.Data.PackedVector         as PV
+import HaskellWorks.Data.Positioning
+import HaskellWorks.Data.Take
+import Prelude                                hiding (length, take)
+import Safe
+
+import qualified Data.Vector.Storable as DVS
 
 data EliasFano64 = EliasFano64
-  { hi      :: DVS.Vector Word64
-  , lo      :: PackedVector64
-  , loBits  :: Int
-  , count   :: Count
+  { efHi     :: DVS.Vector Word64
+  , efLo     :: PackedVector64
+  , efLoBits :: Int
+  , efCount  :: Count
   } deriving (Eq, Show)
+
+class FromListWord64 a where
+  fromList :: [Word64] -> a
+
+class ToListWord64 a where
+  toList :: a -> [Word64]
 
 class FromEliasFano64 a where
   fromEliasFano64 :: EliasFano64 -> a
@@ -31,30 +40,30 @@ class FromEliasFano64 a where
 class ToEliasFano64 a where
   toEliasFano64 :: a -> EliasFano64
 
-instance ToEliasFano64 [Word64] where
-  toEliasFano64 ws = case lastMay ws of
+instance FromListWord64 EliasFano64 where
+  fromList ws = case lastMay ws of
     Just end' -> EliasFano64
-      { hi      = DVS.fromList (packToWord64 (packToWord32 (packToWord16 (packToWord8 (mkHiBits loBits' ws)))))
-      , lo      = PV.fromList loBits' ws
-      , loBits  = fromIntegral loBits'
-      , count   = length'
+      { efHi      = DVS.fromList (packToWord64 (packToWord32 (packToWord16 (packToWord8 (mkHiBits loBits' ws)))))
+      , efLo      = PV.fromList loBits' ws
+      , efLoBits  = fromIntegral loBits'
+      , efCount   = length'
       }
       where length' = length ws
             loBits' = fromIntegral (log2 (end' `div` length')) :: Count
     Nothing -> EliasFano64
-      { hi      = DVS.empty
-      , lo      = PV.empty
-      , loBits  = 0
-      , count   = 0
+      { efHi      = DVS.empty
+      , efLo      = PV.empty
+      , efLoBits  = 0
+      , efCount   = 0
       }
 
-instance FromEliasFano64 [Word64] where
-  fromEliasFano64 ef = gen `fmap` take (count ef) [0 ..]
+instance ToListWord64 EliasFano64 where
+  toList ef = gen `fmap` take (efCount ef) [0 ..]
     where gen :: Int -> Word64
-          gen i = let pos             = (loBits ef * i)                                         in
-                  let (index, offset) = pos `quotRem` 64                                        in
-                  let loValue         = (lo ef !!! fromIntegral index) .>. fromIntegral offset  in
-                  let hiValue         = (lo ef !!! fromIntegral index) .>. fromIntegral offset  in
+          gen i = let pos             = (efLoBits ef * i)                                         in
+                  let (index, offset) = pos `quotRem` 64                                          in
+                  let loValue         = (efLo ef !!! fromIntegral index) .>. fromIntegral offset  in
+                  let hiValue         = (efLo ef !!! fromIntegral index) .>. fromIntegral offset  in
                   loValue + hiValue
 
 -- instance AtIndex EliasFano64 where
