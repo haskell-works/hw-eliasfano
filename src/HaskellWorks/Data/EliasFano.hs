f7dec851e27aaad4d35e64bddcf2e54116331908
John Ky <newhoggy@gmail.com>
2017-07-30 23:57:17 +1000

    Failing test

--- src/HaskellWorks/Data/EliasFano.hs
+++ src/HaskellWorks/Data/EliasFano.hs
@@ -9,22 +9,24 @@ module HaskellWorks.Data.EliasFano
   , bucketBitsToHiSegment
   ) where
 
-import Data.Bits                      (countLeadingZeros, finiteBitSize)
+import Data.Bits                        (countLeadingZeros, finiteBitSize)
 import Data.Int
 import Data.Word
-import HaskellWorks.Data.AtIndex      hiding (end)
+import HaskellWorks.Data.AtIndex        hiding (end)
 import HaskellWorks.Data.Bits.BitWise
 import HaskellWorks.Data.Bits.Log2
 import HaskellWorks.Data.FromListWord64
 import HaskellWorks.Data.Positioning
 import HaskellWorks.Data.ToListWord64
-import Prelude                        hiding (length, take)
+import Prelude                          hiding (length, take)
 import Safe
 
+import qualified Prelude as P
+import qualified Data.Vector.Storable                          as DVS
 import qualified HaskellWorks.Data.PackedVector.PackedVector64 as PV
 
 data EliasFano = EliasFano
-  { efBucketBits :: [Bool]            -- 1 marks bucket, 0 marks skip to next
+  { efBucketBits :: DVS.Vector Word64 -- 1 marks bucket, 0 marks skip to next
   , efLoSegments :: PV.PackedVector64 -- Lower segment of each entry
   , efLoBitCount :: Count             -- Number of bits in each lower segment
   , efCount      :: Count             -- Number of entries
@@ -36,14 +38,35 @@ divup n d = fromIntegral (-((-sn) `div` sd)) :: Word64
   where sd = fromIntegral d :: Int64
         sn = fromIntegral n :: Int64
 
+bucketBoolsToBucketWords :: [Bool] -> DVS.Vector Word64
+bucketBoolsToBucketWords bs = DVS.unfoldrN ((P.length bs `div` 64) + 1) gen bs
+  where gen :: [Bool] -> Maybe (Word64, [Bool])
+        gen cs = if not (null cs) then genWord cs 0 0 else Nothing
+        genWord :: [Bool] -> Count -> Word64 -> Maybe (Word64, [Bool])
+        genWord (True :cs) i acc | i < 64 = genWord cs (i + 1) (acc .|. (1 .<. i))
+        genWord (False:cs) i acc | i < 64 = genWord cs (i + 1)  acc
+        genWord        cs  _ acc          = Just (acc, cs)
+
+bucketWordsToBucketBools :: Count -> DVS.Vector Word64 -> [Bool]
+bucketWordsToBucketBools n v = fst (DVS.foldl go (id, n) v) []
+  where go :: ([Bool] -> [Bool], Count) -> Word64 -> ([Bool] -> [Bool], Count)
+        go (bs, c) w | c > 0 = case goWord (c `min` 64) 64 w of
+                                (cs, finalCount) -> (bs . cs, finalCount)
+        go (bs, _) _         = (bs, 0)
+        goWord :: Count -> Count -> Word64 -> ([Bool] -> [Bool], Count)
+        goWord c i w | c > 0 && i > 0 = let b = (w .&. 1) /= 0
+                                        in case goWord (if b then c - 1 else c) (i - 1) (w .>. 1) of
+                                              (bs, finalCount) -> ((b:) . bs, finalCount)
+        goWord c _ _                  = (id, c)
+
 hiSegmentToBucketBits :: Word64 -> [Word64] -> [Bool]
 hiSegmentToBucketBits lastWord = go 0
   where go :: Word64 -> [Word64] -> [Bool]
         go i []     | i >= lastWord = []
         go i (a:as) | i == a        = True:go i as
         go i (a:as) | i <  a        = False:go (i + 1) (a:as)
-        go i []                     = False:go (i + 1) []
-        go _ (_:_)                  = error "Invalid entry"
+        go i []     = False:go (i + 1) []
+        go _ (_:_)  = error "Invalid entry"
 
 bucketBitsToHiSegment :: [Bool] -> [Word64]
 bucketBitsToHiSegment = go 0
@@ -55,7 +78,7 @@ bucketBitsToHiSegment = go 0
 instance FromListWord64 EliasFano where
   fromListWord64 ws = case lastMay ws of
     Just end' -> EliasFano
-      { efBucketBits  = hiSegmentToBucketBits (bucketEnd - 1) his
+      { efBucketBits  = bucketBoolsToBucketWords (hiSegmentToBucketBits (bucketEnd - 1) his)
       , efLoSegments  = PV.fromList loBits' los
       , efLoBitCount  = loBits'
       , efCount       = length'
@@ -69,15 +92,16 @@ instance FromListWord64 EliasFano where
             hiEnd     = end' .>. loBits'
             bucketEnd = 1 .<. fromIntegral (finiteBitSize hiEnd - countLeadingZeros hiEnd) :: Word64
     Nothing -> EliasFano
-      { efBucketBits  = []
+      { efBucketBits  = DVS.empty
       , efLoSegments  = PV.empty
       , efLoBitCount  = 0
       , efCount       = 0
       }
 
 instance ToListWord64 EliasFano where
-  toListWord64 ef = uncurry combine <$> zip (bucketBitsToHiSegment (efBucketBits ef)) (PV.toList (efLoSegments ef))
+  toListWord64 ef = uncurry combine <$> zip (bucketBitsToHiSegment bucketBits) (PV.toList (efLoSegments ef))
     where combine hi lo = (hi .<. efLoBitCount ef) .|. lo
+          bucketBits = bucketWordsToBucketBools (efCount ef) (efBucketBits ef)
 
 -- instance AtIndex EliasFano where
 --   (!!!)   v i = v !! fromIntegral i
--- test/HaskellWorks/Data/EliasFanoSpec.hs
+++ test/HaskellWorks/Data/EliasFanoSpec.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns         #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
 
 module HaskellWorks.Data.EliasFanoSpec (spec) where
 
@@ -8,60 +9,35 @@ import HaskellWorks.Hspec.Hedgehog
 import Hedgehog
 import Test.Hspec
 
+import qualified Data.Vector.Storable                          as DVS
 import qualified HaskellWorks.Data.PackedVector.PackedVector64 as PV
 import qualified Hedgehog.Gen                                  as G
 import qualified Hedgehog.Range                                as R
+
+import Debug.Trace
 
 {-# ANN module ("HLint: Ignore Redundant do" :: String) #-}
 
 spec :: Spec
 spec = describe "HaskellWorks.Data.EliasFanoSpec" $ do
-  it "List to EliasFano" $ require $ withTests 1 $ property $ do
+  xit "List to EliasFano" $ require $ withTests 1 $ property $ do
     ws <- forAll $ pure $ [2, 3, 5, 7, 11, 13, 24]
     let actual = fromListWord64 ws
     let expected = EliasFano
-          { efBucketBits  = [ True
-                            , True
-                            , False
-                            , True
-                            , True
-                            , False
-                            , True
-                            , False
-                            , True
-                            , False
-                            , False
-                            , False
-                            , True
-                            , False
-                            ]
+          { efBucketBits  = DVS.fromList [4443]
           , efLoSegments  = PV.fromList 2 [2, 3, 1, 3, 3, 1, 0]
           , efLoBitCount  = 2
           , efCount       = 7
           }
     actual === expected
-  it "hiSegment <-> bucketBits round trip" $ require $ property $ do
+  xit "hiSegment <-> bucketBits round trip" $ require $ property $ do
     vs <- forAll $ G.list (R.linear 1 100) (G.word64 (R.linear 0 20))
     ws <- forAll $ pure $ drop 1 $ scanl (+) 0 vs
     maxW <- forAll $ pure $ last ws
     bucketBitsToHiSegment (hiSegmentToBucketBits maxW ws) === ws
-  it "List to EliasFano" $ require $ withTests 1 $ property $ do
+  xit "List to EliasFano" $ require $ withTests 1 $ property $ do
     ws <- forAll $ pure $ EliasFano
-          { efBucketBits  = [ True
-                            , True
-                            , False
-                            , True
-                            , True
-                            , False
-                            , True
-                            , False
-                            , True
-                            , False
-                            , False
-                            , False
-                            , True
-                            , False
-                            ]
+          { efBucketBits  = DVS.fromList [4443]
           , efLoSegments  = PV.fromList 2 [2, 3, 1, 3, 3, 1, 0]
           , efLoBitCount  = 2
           , efCount       = 7
@@ -71,6 +47,11 @@ spec = describe "HaskellWorks.Data.EliasFanoSpec" $ do
     actual === expected
   it "Round trip" $ require $ property $ do
     vs <- forAll $ G.list (R.linear 0 100) (G.word64 (R.linear 1 20))
-    ws <- forAll $ pure $ drop 1 $ scanl (+) (-1) vs
+    let !_ = trace ("vs: " ++ show vs) ()
+    ws <- forAll $ pure $ drop 1 $ scanl (+) 0 vs
+    let !_ = trace ("ws: " ++ show ws) ()
     ef :: EliasFano <- forAll $ pure $ fromListWord64 ws
-    toListWord64 ef === (ws :: [Word64])
+    let !_ = trace ("ef: " ++ show ef) ()
+    let actual = toListWord64 ef
+    let !_ = trace ("actual: " ++ show actual) ()
+    actual === (ws :: [Word64])
