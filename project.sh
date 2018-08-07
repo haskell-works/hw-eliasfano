8a9aba74ddd27a49d638e6861135307dfb51c11d
John Ky <newhoggy@gmail.com>
2018-08-07 21:35:41 +1000

    Add build via Visual Studio Code support

---
+++ project.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+STACK_FLAGS=""
+
+case $1 in
+  build)
+    stack build \
+      --test --no-run-tests --bench --no-run-benchmarks \
+      $STACK_FLAGS
+    ;;
+
+  test)
+    stack test \
+      $STACK_FLAGS
+    ;;
+
+  bench)
+    stack bench \
+      $STACK_FLAGS
+    ;;
+
+  repl)
+    stack repl \
+      $STACK_FLAGS
+    ;;
+esac
